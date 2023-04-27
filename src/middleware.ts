1b55363106927a0148940ac20d6150d56a9fdf9d
Thomas Beckford <tebeckford@gmail.com>
2023-04-26 21:40:45 -0300

    feat add authentication

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+ 
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
+ 
+export const config = {
+  matcher: [
+
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
+}
